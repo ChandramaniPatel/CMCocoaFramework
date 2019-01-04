a90f5fc0f6fec8c76c9205808ac316454bd0265d
Chandramani <Chandramani1983@gmail.com>
2019-01-04 11:52:54 +0530

    framework is added

---
+++ CMFramework/CMFramework/CMFramework.h
@@ -0,0 +1,19 @@
+//
+//  CMFramework.h
+//  CMFramework
+//
+//  Created by Chandramani on 04/01/19.
+//  Copyright © 2019 Chandramani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMFramework.
+FOUNDATION_EXPORT double CMFrameworkVersionNumber;
+
+//! Project version string for CMFramework.
+FOUNDATION_EXPORT const unsigned char CMFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMFramework/PublicHeader.h>
+
+
---
+++ CMFramework/CMFramework/Services.swift
@@ -0,0 +1,18 @@
+//
+//  Services.swift
+//  CMFramework
+//
+//  Created by Chandramani on 04/01/19.
+//  Copyright © 2019 Chandramani. All rights reserved.
+//
+
+import Foundation
+
+public class Services {
+    
+    private init()  {}
+    
+    public static func doSomething()-> String {
+        return "Creating cocoa framwork for code sharing"
+    }
+}
